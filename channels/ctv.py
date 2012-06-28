81e885ae84bffcb750e2161c14f93b0427e2d3fb
Marius Muja <mariusm@cs.ubc.ca>
2012-06-27 23:36:53 -0700

    Place multipart news in a single sub-folder

--- channels/ctv.py
+++ channels/ctv.py
@@ -193,11 +193,9 @@ class CTVNews(CTVBaseChannel):
     default_action = 'browse'
 
     def action_browse(self):
-        if self.args['remote_url']:
-            remote_url = self.args['remote_url']
-        else:
-            remote_url = self.base_url            
-        soup = BeautifulSoup(self.plugin.fetch(remote_url, max_age=self.cache_timeout))
+        if not self.args['remote_url']:
+            self.args['remote_url'] = self.base_url
+        soup = BeautifulSoup(self.plugin.fetch(self.args['remote_url'], max_age=self.cache_timeout))
 
         for category in soup.findAll('dt', {'class': 'videoPlaylistCategories'}):
             data = {}
@@ -206,52 +204,69 @@ class CTVNews(CTVBaseChannel):
                 'action': 'browse_category',
                 'Title': category.a.contents[0],
                 'entry_id': None,
-                'remote_url': remote_url+"/"+category['id']+'?ot=example.AjaxPageLayout.ot&maxItemsPerPage=12',
+                'category_id': category['id'],
                 'page_num' : 1
             })
             self.plugin.add_list_item(data)
         self.plugin.end_list()
 
     def action_browse_category(self):
-        soup = BeautifulSoup(self.plugin.fetch(self.args['remote_url']+"&"+"pageNum="+self.args['page_num'], max_age=self.cache_timeout))
-
+        soup = BeautifulSoup(self.plugin.fetch("%s/%s?ot=example.AjaxPageLayout.ot&maxItemsPerPage=12&pageNum=%s"%(self.args['remote_url'],self.args["category_id"],self.args['page_num']), 
+                        max_age=self.cache_timeout))
         #print soup
         for clip in soup.findAll('article', {'class':'videoPackageThumb'}):
             #print clip
             thumb = clip.img['src']
             tagline = clip.h3.string
+            #title = clip.find('p',{'class':'videoPlaylistDescription'}).string
             
             script = clip.findNextSibling()
+            scripts = []
             while script:
                 if script.name!='script': break;
-                #print script
-                try:
-                    txt = script.string.strip()
-                except:
-                    pass
-
-                if txt.find('clip.id')>0:
-                    match = re.search('.*clip[.]id = ([0-9]*).*clip[.]title = escape\("(.*)"\).*clip[.]description = escape\("(.*)"\).*',txt,re.DOTALL)
-                    clipId = match.group(1)
-                    title = match.group(2).strip()
-                    plot = match.group(3).strip()
-                    
-                    #print thumb
+                scripts.append(script)
+                script = script.findNextSibling()
+            
+            if len(scripts)>2:
+                script = scripts[0]
+                txt = script.string.strip()
+                if txt.find('playlistMap[')>=0:
+                    match = re.search("playlistMap\['([0-9.]*)'\] = new Array()",txt)
+                    playlist_id = match.group(1)
                     data = {}
                     data.update(self.args)
                     data.update({
-                        'Title': title,
-                        'action': 'play_clip',
-                        'remote_url': clipId,
-                        'clip_id': clipId,
+                        'action': 'browse_playlist',
+                        'Title': tagline,
+                        'entry_id': None,
                         'Thumb': thumb,
-                        'tagline': tagline,
-                        'plot': plot,
-                        'genre': 'News'
+                        'playlist_id': playlist_id,
                     })
-                    self.plugin.add_list_item(data, is_folder=False)
-                script = script.findNextSibling()
-        
+                    self.plugin.add_list_item(data)
+            else:
+                for script in scripts:
+                    txt = script.string.strip()
+                    if txt.find('clip.id')>=0:
+                        match = re.search('.*clip[.]id = ([0-9]*).*clip[.]title = escape\("(.*)"\).*clip[.]description = escape\("(.*)"\).*',txt,re.DOTALL)
+                        clipId = match.group(1)
+                        title = match.group(2).strip()
+                        plot = match.group(3).strip()
+                        
+                        #print thumb
+                        data = {}
+                        data.update(self.args)
+                        data.update({
+                            'Title': title,
+                            'action': 'play_clip',
+                            'remote_url': clipId,
+                            'clip_id': clipId,
+                            'Thumb': thumb,
+                            'tagline': tagline,
+                            'plot': plot,
+                            'genre': 'News'
+                        })
+                        self.plugin.add_list_item(data, is_folder=False)
+            
         nextPager = soup.find("span", {"class":"videoPaginationNext"})
         if nextPager and nextPager.find('a'):
             data = {}
@@ -262,8 +277,31 @@ class CTVNews(CTVBaseChannel):
             })
         self.plugin.add_list_item(data)    
         
-        self.plugin.end_list()        
+        self.plugin.end_list()
 
+    def action_browse_playlist(self):
+        soup = BeautifulSoup(self.plugin.fetch("%s/%s?ot=example.AjaxPageLayout.ot&maxItemsPerPage=12&pageNum=%s"%(self.args['remote_url'],self.args["playlist_id"],self.args['page_num']), 
+                        max_age=self.cache_timeout))
+        for clip in soup.findAll('article', {'class':'videoClipThumb'}):
+            #print clip
+            thumb = clip.img['src']
+            tagline = clip.h3.a.string
+            clipId = clip['id']
+            plot = clip.p.string.strip()
+            data = {}
+            data.update(self.args)
+            data.update({
+                'Title': tagline,
+                'action': 'play_clip',
+                'remote_url': clipId,
+                'clip_id': clipId,
+                'Thumb': thumb,
+                'tagline': tagline,
+                'plot': plot,
+                'genre': 'News'
+            })
+            self.plugin.add_list_item(data, is_folder=False)
+        self.plugin.end_list()
 
 class CTVLocalNews(CTVNews):
     short_name = 'ctvlocal'
